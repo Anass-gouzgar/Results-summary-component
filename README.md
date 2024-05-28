40f3d5f35045734ea9ee0c232a700cb5bf5d91bb
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-05-28 21:54:00 +0100

    first init

--- README.md
+++ README.md
@@ -1,36 +1,48 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Frontend Mentor - Results summary component solution
 
-## Getting Started
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-First, run the development server:
+## Table of contents
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Author](#author)
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+## Overview
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+### The challenge
 
-## Learn More
+Users should be able to:
 
-To learn more about Next.js, take a look at the following resources:
+- See hover states for interactive elements
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
 
-## Deploy on Vercel
+### Links
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+- Solution URL: (https://github.com/Anass-gouzgar/Results-summary-component)
+- Live Site URL:()
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Next.js](https://nextjs.org/) - React framework
+- [Styled Components](https://styled-components.com/) - For styles
+
+
+## Author
+
+- Website - [GOUZGAR](https://anassgouzgar.com)
+- Frontend Mentor - [@Anass-gouzgar](https://www.frontendmentor.io/profile/Anass-gouzgar)
+- Twitter - [@AGouzgar](https://x.com/AGouzgar)
+
---
+++ src/app/components/Card/index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Image from "next/image";
+import { data } from "../../../../data/data.js";
+
+const Index = () => {
+  return (
+    <div className="flex flex-col md:flex md:flex-row rounded-3xl md:overflow-hidden md:w-[800px] md:h-[520px] bg-white shadow-2xl">
+      {/* first card */}
+      <div className="flex flex-col p-4 md:p-0 flex-1 justify-center items-center gap-y-9 md:rounded-3xl rounded-b-3xl bg-gradient-to-b from-gradient-lightSlateBlue to-gradient-lightRoyalBlue">
+        <h2 className="text-neutral-lightLavender md:text-2xl text-xl font-bold">
+          Your Result
+        </h2>
+        <div className="flex flex-col justify-center items-center gap-3 rounded-full w-52 aspect-[1/1] bg-gradient-to-b from-gradient-violetBlue">
+          <h1 className="text-white text-7xl font-bold">76</h1>
+          <p className="text-neutral-lightLavender/60 text-lg font-semibold">
+            of 100
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-4">
+          <h2 className="text-3xl text-white font-bold">Great</h2>
+          <p className="text-center text-neutral-lightLavender text-lg w-[90%] md:w-[65%]">
+            You scored higher than 65% of the people who have taken these tests.
+          </p>
+        </div>
+      </div>
+
+      {/* second card */}
+      <div className="flex-1">
+        <div className="p-6 flex flex-col h-full w-full">
+          <h1 className="md:text-3xl text-xl text-black font-semibold">Summary</h1>
+
+          {data?.map((item, index) => (
+            <div
+              key={index}
+              className={`flex justify-between items-center gap-x-4f mt-4 p-3 rounded-xl bg-opacity-5  ${item.bgColor}`}
+            >
+              <div className="flex gap-x-2 items-center">
+                <Image
+                  src={item.icon}
+                  className="w-6 h-10"
+                  width={0}
+                  height={0}
+                  alt="icon"
+                />
+                <p className={`text-lg ${item.color} font-semibold`}>
+                  {item.category}
+                </p>
+              </div>
+              <div className="flex gap-x-2 items-center">
+                <p className="text-lg text-black font-bold">{item.score}</p>
+                <p className="text-lg text-black/50 font-semibold">/ 100</p>
+              </div>
+            </div>
+          ))}
+{/* <div className="w"> */}
+              <button className="bg-black/80 text-white rounded-3xl w-full py-4 mt-10 font-semibold hover:bg-gradient-lightRoyalBlue">Continue</button>
+
+{/* </div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
